1f8b77810cdf50ee955e1ad2e4de22bd7855bee9
Mark Bakker <mark.bakker@koop.overheid.nl>
2022-11-04 10:51:07 +0100

    Fixed the invalid next.js config for static export

--- next.config.js
+++ next.config.js
@@ -2,6 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    unoptimized: true,
+  },
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      '/': { page: '/' },    }
+  },
 }
 
 module.exports = nextConfig
